18f21e994c8b99580e5f2c7d827f0f2a8d03615c
20230960-DoYeonLee <20230960@vision.hoseo.edu>
2026-03-25 13:04:05 +0900

    Add files via upload

---
+++ 20230960_stack_report1.c
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    char str[101]; // 입력 문자열 (최대 100자)
+    char* stack; // 스택
+    int top = -1;
+    int length;
+
+    printf("문자열 입력: "); //문자열 입력
+    fgets(str, sizeof(str), stdin); //scanf는 띄여쓰기 불가
+
+    str[strcspn(str, "\n")] = '\0'; //개행 제거 (안하면 \n도 한칸을 먹어서 문자열을 99까지밖에 못씀)
+
+    length = strlen(str);
+
+    stack = (char*)malloc(sizeof(char) * length); //스택 메모리 동적 할당
+
+    for (int i = 0; i < length; i++) {
+        stack[++top] = str[i];
+    }
+
+    printf("거꾸로 출력: "); //pop 하면서 출력 (역순으로)
+    while (top != -1) {
+        printf("%c", stack[top--]);
+    }
+
+    printf("\n");
+
+    free(stack); // 메모리 해제
+
+    return 0;
+}
---
+++ 20230960_stack_report2.c
@@ -0,0 +1,46 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    char str[101];
+    char stack[101];
+    int top = -1;
+
+    printf("문자열을 입력하세요: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+
+        // 여는 괄호 push
+        if (str[i] == '(' || str[i] == '{' || str[i] == '[') {
+            stack[++top] = str[i];
+        } 
+
+        // 닫는 괄호 검사
+        else if (str[i] == ')' || str[i] == '}' || str[i] == ']') {
+
+            if (top == -1) {
+                printf("유효하지 않은 괄호입니다.\n");
+                return 0;
+            }
+
+            char open = stack[top--];
+
+            if ((str[i] == ')' && open != '(') ||
+                (str[i] == '}' && open != '{') ||
+                (str[i] == ']' && open != '[')) {
+                printf("유효하지 않은 괄호입니다.\n");
+                return 0;
+            }
+        }
+    }
+
+    if (top == -1)
+        printf("유효한 괄호입니다.\n");
+    else
+        printf("유효하지 않은 괄호입니다.\n");
+
+    return 0;
+}
